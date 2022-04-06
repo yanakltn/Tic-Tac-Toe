f35eb0849b4246243b2b2c315907c3f85b89db43
Yana Kalytyn <masandralive2@gmail.com>
2022-04-06 19:54:17 +0300

    completed tic tac toe project

---
+++ src/components/Square.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const style = {
+  background: "lightblue",
+  border: "2px soldi darkblue",
+  fontSize: "30px",
+  fontWeight: "800",
+  cursor: "pointer",
+  outline: "none",
+};
+
+const Square = ({ value, onClick }) => (
+  <button style={style} onClick={onClick}>
+    {value}
+  </button>
+);
+export default Square;
